1fc2a4a314d3c01a5f5f39bad61e7e9df64e4586
Matthias Hellmann <hellmann@uni-koeln.de>
2021-11-09 14:51:52 +0100

    updated debug files for vivado 2021.2; helper scripts

---
+++ sw/orkagd/src/spec503/reconfigure.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /opt/Xilinx/Vivado/2018.2/settings64.sh && \
+../../../../hw/xilinx/configure_fpga "bitstream.bit"
---
+++ sw/orkagd/src/spec503/reload_driver.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pushd ~/arbeit/orka/github-orka-hpc/orkadistro/llp_rrzk/rrzk_llp_repo/3rdparty/xilinx_dma_drivers/XDMA/linux-kernel/tests/
+
+sudo ./load_driver.sh
+
+popd
